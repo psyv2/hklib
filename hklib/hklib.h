8cd00ca7c94c2e8a980af6279c127f499300e4f8
Harshit Kushwah <harshitkushwah@Administrators-MacBook-Pro.local>
2024-08-13 15:01:16 +0530

    intial setup

---
+++ hklib/hklib.h
@@ -0,0 +1,18 @@
+//
+//  hklib.h
+//  hklib
+//
+//  Created by Harshit Kushwah on 13/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for hklib.
+FOUNDATION_EXPORT double hklibVersionNumber;
+
+//! Project version string for hklib.
+FOUNDATION_EXPORT const unsigned char hklibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hklib/PublicHeader.h>
+
+
---
+++ hklib/swiftylib.swift
@@ -0,0 +1,21 @@
+//
+//  swiftylib.swift
+//  hklib
+//
+//  Created by Harshit Kushwah on 13/08/24.
+//
+
+import Foundation
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
